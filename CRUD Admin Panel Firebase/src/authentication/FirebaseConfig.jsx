51369b96daa555d7899fcad33ff28cc72e3d01ef
Muhammad Ali Minhas <minhasvirtual@gmail.com>
2024-08-14 13:57:52 +0500

    Update FirebaseConfig.jsx

--- CRUD Admin Panel Firebase/src/authentication/FirebaseConfig.jsx
+++ CRUD Admin Panel Firebase/src/authentication/FirebaseConfig.jsx
@@ -3,14 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDi_9tec8AnkueOGue7UiV_-VeFs6HOpBE",
-  authDomain: "adminconsoleproject-d658d.firebaseapp.com",
-  projectId: "adminconsoleproject-d658d",
-  storageBucket: "adminconsoleproject-d658d.appspot.com",
-  messagingSenderId: "1005611556330",
-  appId: "1:1005611556330:web:e58d4faace06ed2af9a693"
-};
+// put your app settings here 
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
